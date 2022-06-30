fb3d386131b9c56b34c489ddea359f49658d30ab
tjalsgur0083 <tjalsgur0083@gmail.com>
2022-06-30 17:09:30 +0900

    baekjun

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 using namespace std;
 
+int fibo(int n)
+{
+    if(n == 0)
+    {
+        return 0;
+    }
+    else if(n == 1)
+    {
+        return 1;
+    }
+    else if(n == 2)
+    {
+        return 1;    
+    }
+    else
+    {
+        return fibo(n - 1) + fibo(n-2);
+    }
+}
 int main()
 {
+    int num = 0;
     
+     cin >> num;
+    cout << fibo(num) << endl;
 }
