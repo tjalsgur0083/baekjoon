4b3c22f1ddae69f9c73f4751651787d91419987d
tjalsgur0083 <tjalsgur0083@gmail.com>
2022-07-07 14:19:21 +0900

    baekjun 8958

---
+++ OX_Quizzes.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int num = 0, score = 0, total = 0;
+    string test_case = "";
+    vector<string> v;
+    vector<int> final_output;
+
+    cin >> num;
+    for(int i = 0; i < num; i++)
+    {
+        cin >> test_case;
+        v.push_back(test_case);
+
+        total = 0;
+        score = 0;
+        test_case = v[i];
+
+        for(int j = 0; j < test_case.length(); j++)
+        {
+            if(test_case[j] == 'O')
+            {
+                score++;
+                total += score;
+            }
+            else
+            {
+                score = 0;
+            }
+        }
+        final_output.push_back(total);
+    }
+    for(int i = 0; i < final_output.size(); i++)
+    {
+        cout << final_output.at(i) << endl;
+    }
+}
