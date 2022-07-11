096e56e1cff73eb87e3b851ea06b69485ad27e07
tjalsgur0083 <tjalsgur0083@gmail.com>
2022-07-11 15:20:24 +0900

    baekjun 1436

---
+++ movie_director_syom.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N = 0, num = 0, name = 0, temp = 0;
+    cin >> N;
+
+    while(num != N)
+    {
+        name++;
+        temp = name;
+
+        while(temp != 0)
+        {
+            if(temp % 1000 == 666)
+            {
+                num++;
+                break;
+            }
+            else
+            {
+                temp /= 10;
+            }
+        }
+    }
+    cout << name << endl;
+} 
