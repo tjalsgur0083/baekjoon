ac8fdbf08b2407d07bdf1839624262e9406aba77
tjalsgur0083 <tjalsgur0083@gmail.com>
2022-07-12 17:36:40 +0900

    baekjun 2231

---
+++ decompose.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 0, total = 0;
+    int count = 0;
+
+    cin >> num;
+    
+    for(int i = 1; i < num; i++)
+    {
+        total = i;
+        count = i;
+
+        while(count)
+        {
+            total = total + count % 10;
+            count /= 10;
+        }
+
+        if(num == total)
+        {
+            cout << i << endl;
+            return 0;
+        }
+    }
+    cout << 0 << endl;
+    return 0;
+}
