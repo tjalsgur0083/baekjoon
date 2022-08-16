2b2363d34a3a359a753c2991d67074daa87115c8
tjalsgur0083 <tjalsgur0083@gmail.com>
2022-08-16 20:37:41 +0900

    baekjoon 1008

---
+++ A÷B.cpp
@@ -0,0 +1,15 @@
+//baekjoon 1008
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	double a, b;
+	cin >> a >> b;
+	cout << fixed;
+	cout.precision(9);
+	cout << a / b << endl;
+
+	return 0;
+}
