5c1168b4f9f988f44377088a71adf74660ecafb5
seominhyeok <33335744+tjalsgur0083@users.noreply.github.com>
2022-09-13 17:13:17 +0900

    Update fibonacci_number_1.cpp

--- fibonacci_number_1.cpp
+++ fibonacci_number_1.cpp
@@ -23,7 +23,7 @@ int main()
     arr[1] = arr[2] = 1;
     for(int i = 3; i <= n; i++)
     {
-        arr[1] = arr[i - 1] + arr[i - 2];
+        arr[i] = arr[i - 1] + arr[i - 2];
         cnt2++;
     }
     cout << cnt1 << " " << cnt2;
